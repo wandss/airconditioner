5d63e35e994ccb7e004aeab9937d6151a5c46bd4
wand <wandss@gmail.com>
2022-04-25 16:34:02 -0300

    update docker-compose-prod

--- frontend/airconditioner/vue.config.js
+++ frontend/airconditioner/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'? '/frontend/':'/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/frontend/' : '/',
   transpileDependencies: true,
   productionSourceMap: false,
   devServer: {
